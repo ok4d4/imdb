2e3b83c73dc08a275ad32208f89af5f37b21a114
ok4d4 <okap.dsweb@gmail.com>
2023-04-09 16:11:45 +0900

    text-red-700

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>test</h1>;
+  return <h1 className="text-red-700">test</h1>;
 }
