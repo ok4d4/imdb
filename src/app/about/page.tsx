bbc27a277567cc85fb36d0399b4fb9da9477eede
ok4d4 <okap.dsweb@gmail.com>
2023-04-09 21:10:04 +0900

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// about page
+// Name, description, and links to social media
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-500">About</h1>
+      <p className="">これはサービスの説明文です</p>
+      <p className="">これはサービスの説明文です</p>
+    </div>
+  );
+}
